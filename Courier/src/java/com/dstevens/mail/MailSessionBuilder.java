7b81b383a3e51e62a02d24a9f8f940734b8ce117
wastevens <wastevens@gmail.com>
2015-07-29 19:48:18 -0400

    Overhauled the Courier session building, to be more flexable and extensible.

---
+++ Courier/src/java/com/dstevens/mail/MailSessionBuilder.java
@@ -0,0 +1,73 @@
+package com.dstevens.mail;
+
+import java.util.Properties;
+import java.util.function.Supplier;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
+public class MailSessionBuilder {
+
+	private String smtp = "";
+	private String username = "";
+	private String password = "";
+	private int port = -1;
+	private boolean startTLS = false;
+	private Class<?> socketFactoryClass;
+
+	public MailSessionBuilder withSMTP(String smtp) {
+		this.smtp = smtp;
+		return this;
+	}
+	
+	public MailSessionBuilder withUsername(String username) {
+		this.username = username;
+		return this;
+	}
+	
+	public MailSessionBuilder withPassword(String password) {
+		this.password = password;
+		return this;
+	}
+	
+	public MailSessionBuilder withPort(int port) {
+		this.port = port;
+		return this;
+	}
+	
+	public MailSessionBuilder startTLS(boolean startTLS) {
+		this.startTLS = startTLS;
+		return this;
+	}
+	
+	public MailSessionBuilder withSocketFactoryClass(Class<?> socketFactoryClass) {
+		this.socketFactoryClass = socketFactoryClass;
+		return this;
+	}
+	
+	public Supplier<Session> build() {
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls.enable", Boolean.toString(startTLS));
+		properties.put("mail.smtp.socketFactory.class", socketFactoryClass.getName());
+		properties.put("mail.smtp.host", smtp);
+		properties.put("mail.smtp.port", port);
+		
+		Authenticator authenticator = new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		};
+		
+		return new Supplier<Session>() {
+			@Override
+			public Session get() {
+				return Session.getInstance(properties, authenticator);
+			}
+		};
+	}
+	
+	
+	
+}
---
+++ Courier/src/test/integration/java/MailMessageTest.java
@@ -0,0 +1,32 @@
+import java.util.function.Supplier;
+
+import javax.mail.Session;
+import javax.net.ssl.SSLSocketFactory;
+
+import org.junit.Test;
+
+import com.dstevens.mail.MailMessage;
+import com.dstevens.mail.MailSessionBuilder;
+
+
+public class MailMessageTest {
+
+	@Test
+	public void testSendMailMessageViaGMail() {
+		Supplier<Session> supplier = 
+				new MailSessionBuilder().withSMTP("smtp.gmail.com").
+								 withPort(465).
+								 withSocketFactoryClass(SSLSocketFactory.class).
+								 withUsername("wastevens@gmail.com").
+								 withPassword("B090182d!").
+								 build();
+		MailMessage message = new MailMessage(supplier.get());
+		
+		message.from("int-test@address.com", "Integration Test From name");
+		message.to("wastevens@gmail.com");
+		message.subject("Integration test email subject");
+		message.body("Integration test email body");
+		message.send();
+	}
+	
+}
