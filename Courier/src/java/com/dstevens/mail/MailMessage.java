42be47646e28820abec266c5fe80ffae1201e82c
wastevens <wastevens@gmail.com>
2015-08-23 12:15:27 -0400

    Added reasonably equals/hashcode/toString to MailMessage

--- Courier/src/java/com/dstevens/mail/MailMessage.java
+++ Courier/src/java/com/dstevens/mail/MailMessage.java
@@ -9,6 +9,10 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 public class MailMessage {
 
 	private Session session;
@@ -59,5 +63,19 @@ public class MailMessage {
         	throw new IllegalStateException("Failed to send message to " + toAddress, e);
         }		
 	}
-
+	
+	@Override
+	public boolean equals(Object that) {
+		return EqualsBuilder.reflectionEquals(this, that);
+	}
+	
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+	
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
 }
